41300437b4b9f8054337bb9c5edfe040a7fcca0c
AsmundH <aasmund.haugse@gmail.com>
2018-11-09 18:25:17 +0100

    Merger server og fikser Client og Server mapper

---
+++ server/routes/product.js
@@ -0,0 +1,51 @@
+//reference customer model:
+import express from 'express';
+import ProductModel from '../models/product.model';
+
+const router = express.Router();
+
+// GET by param, if param unspecified it returns all
+/*
+Specifying the sortBy parameter allows you to chose how the list is sorted:
+  Volum, Pris, Varenummer, Literpris, Fylde, Friskhet, Bitterhet, Sodme, Argang(Noen har ikke Årgang),
+  Alkohol, Sukker, APK
+Specifying the respective keys allows for searching for specific products, for example: Land=Italia
+ Allowed: Varenavn, Varetype, land, Distrikt
+Using && between keys allows for specifying several types
+ */
+
+router.get('/product',(req, res) => {
+  let sorting = req.query.sorting ? req.query.sorting: 'Pris';
+  let order = req.query.order ? req.query.order : '1';
+  let page = req.query.page ? req.query.page : 1;
+  let pages=parseInt(page);
+  let limit = req.query.limit ? req.query.limit : 10;
+  let lim=parseInt(limit);
+
+  let content = {};
+  if (req.query.Varenavn) {
+    content.Varenavn = req.query.Varenavn
+  }
+  if (req.query.Varetype) {
+    content.Varetype = req.query.Varetype
+  }
+  if (req.query.Land) {
+    content.Land = req.query.Land
+  }
+  if (req.query.Distrikt) {
+    content.Distrikt = req.query.Distrikt
+  }
+
+  ProductModel.paginate(content,{
+    page: pages,
+    limit: lim,
+    sort: {[sorting]:[order]}
+  }).then(page => {
+    res.json(page)
+  })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+})
+
+export default router;
---
+++ server/server.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+import customerRoute from './routes/customer'
+import productRoute from './routes/product'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.load({path: '.env'});
+
+app.use(bodyParser.json())
+app.use(productRoute)
+app.use(customerRoute)
+
+app.use((req,res,next) => { 
+  console.log(`${new Date().toString()} => ${req.originalUrl}`,req.body)
+  next()
+})
+
+
+//lokalt:
+const server=process.env.MONGODB_IDI_URI;
+
+//Connect to the database(only done once)
+//mongoose.connect(`mongodb://${user}:${password}@${server}/${database}`)
+mongoose.connect(server, { useNewUrlParser: true } )
+
+
+app.use(customerRoute)
+app.use(productRoute)
+app.use(express.static('public'))
+
+
+// Handler for 404 - Resource not found
+app.use((req,res,next) => {
+  res.status(404).send("We think you are lost!")
+})
+
+// Handler for 500
+app.use((err,req,res,next) => {
+  console.error(err.stack)
+  res.sendFile(path.join(__dirname,'../public/500.html'))
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.info(`Server has started on ${PORT}`))
