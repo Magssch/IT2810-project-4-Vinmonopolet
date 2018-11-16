89862a8c3007426bbb44dcfb533c1480596cd757
Magnus Schjølberg <37403247+Magssch@users.noreply.github.com>
2018-11-16 14:03:15 +0100

    Videre oppdeling av tester #11

--- client/cypress/integration/search_query_spec.js
+++ client/cypress/integration/search_query_spec.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 
 describe('Search', () => {
-    it('searches for an item and receives a specific result set', () => {
+    it('Searches for an item and receives a specific result set', () => {
         // visit 'baseUrl'
         cy.visit('/');
 
@@ -20,6 +20,11 @@ describe('Search', () => {
         // Check if query was successful
         cy.contains('Hennessy X.O.');
 
+    });
+});
+
+describe('Multiple queries', () => {
+    it('Specifies a stricter condition, using multiple queries', () => {
 
         // Query a more specific result
         cy.get('#query-volume').click();
@@ -31,6 +36,11 @@ describe('Search', () => {
         // Do not expect any 0.35 volume bottles
         cy.get('.db-item-field-volume').contains('0.35').should('not.exist');
 
+    });
+});
+
+describe('Empty result', () => {
+    it('Query an expectedly non-exisiting item, check that no results are shown.', () => {
 
         // Query an expected empty result
         cy.get('#query-country').click();
